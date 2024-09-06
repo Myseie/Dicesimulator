66f041dc6c6792e8f7b8335a9dafaf8eb92ae312
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:36:47 +0200

    Add files via upload

---
+++ Dicesimulator/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd  = new Random();
+
+Console.Write("Skriv in hur många gånger du vill kasta tärningarna : ");
+
+string input = Console.ReadLine();
+
+int number  = int.Parse(input);
+
+for (int i = 0; i < number; i++)
+{
+    int dice = rnd.Next(1, 7);
+    int dice2 = rnd.Next(1, 7);
+
+    Console.WriteLine($"Kast {i + 1}: Tärning 1 = {dice}, Tärning 2 = {dice2}");
+}
